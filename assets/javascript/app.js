5724da832d6af7d984870e5bea68ff2ca27fa852
StephenDK <stephendklein08@gmail.com>
2018-01-05 00:28:35 -0800

    Project 1

---
+++ assets/javascript/app.js
@@ -0,0 +1,362 @@
+
+// Section 1:
+// global variables
+// ============================================================================
+
+// Var for tracking the total number of users.
+var totalUsers;
+// Var for tracking the user slot #
+var userName;
+// Boolean for tracking whether or not the browser is signed in as a user
+var isSignedIn = false;
+// Configurable location update time interval in milliseconds
+var locationInterval = 10000;
+// For setting the connected user reference
+var userRef;
+// The user's dynamic key in firebase
+var userKey;
+// Global variable for storing URL of last uploaded image
+var url = "https://res.cloudinary.com/djzxhcr1g/image/upload/v1515035725/bread-2_qyscfk.jpg";
+// Global variable for tracking whether an image is ready
+var imageReady = false;
+
+// Section 2:
+// Firebase CDN
+//  ===========================================================================
+ // Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCXu6muve1SKZFE9W0x7hvz3H_rmCJtgsQ",
+    authDomain: "breadcrumbs-1513133131724.firebaseapp.com",
+    databaseURL: "https://breadcrumbs-1513133131724.firebaseio.com",
+    projectId: "breadcrumbs-1513133131724",
+    storageBucket: "breadcrumbs-1513133131724.appspot.com",
+    messagingSenderId: "769979061333"
+  };
+firebase.initializeApp(config);
+  
+var db = firebase.database();
+var auth = firebase.auth();
+// var user = firebase.auth().currentUser;
+
+function showLoginScreen() {
+	$("#loginScreen").show();
+}
+
+function hideLoginScreen() {
+	$("#loginScreen").hide();
+}
+
+function showLogoutButton() {
+	$("#user-Logout").show();
+}
+
+function hideLogoutButton() {
+	$("#user-Logout").hide();
+}
+
+function showLoginButton() {
+	$("#login").show();
+}
+
+function hideLoginButton() {
+	$("#login").hide();
+}
+
+function showRegisterButton() {
+	$("#register_Me").show();
+}
+
+function hideRegisterButton() {
+	$("#register_Me").hide();
+}
+
+function showItScreen() {
+	$("#itScreen").show()
+}
+
+function hideItScreen() {
+	$("#itScreen").hide();
+}
+
+function showSeekerScreen() {
+	$("#seekerScreen").show();
+}
+
+function hideSeekerScreen() {
+	$("#seekerScreen").hide();
+}
+
+function showTagDiv() {
+	$("#tagDiv").show();
+}
+
+function hideTagDiv() {
+	$("#tagDiv").hide();
+}
+
+function displayScreen() {
+	hideLoginButton();
+	hideRegisterButton();
+	showLogoutButton();
+	if (isIt == true) {
+		showItScreen();
+	}
+	else {
+		showSeekerScreen();
+	}
+}
+
+function hideScreen() {
+	hideItScreen();
+	hideSeekerScreen();
+	hideLogoutButton();
+	showLoginButton();
+	showRegisterButton();
+	showLoginScreen();
+}
+
+// Sets the user variable and signed in boolean upon user sign-in.
+function setUser() {
+	user = firebase.auth().currentUser;
+	isSignedIn = true;
+	checkIfIt();
+	checkIt();
+	connectUser();
+	displayScreen();
+}
+
+function signOutUser() {
+	userRef.remove();
+	isSignedIn = false;
+	isIt = false;
+	userName = "";
+	userRef = "";
+	hideScreen();
+}
+
+// Checks to see if the user is currently signed in.
+function checkUser() {
+	if (user) {
+		console.log("Signed in as user " + user.email);
+		// console.log(user);
+		hideLoginScreen();
+	} else {
+		console.log("No user is signed in");
+	}
+}
+
+// Adds the user's userName to the list of connectedUsers in firebase
+function connectUser() {
+	db.ref('connectedUsers').child(userName).set(true);
+	userRef = db.ref('connectedUsers').child(userName);
+	userRef.onDisconnect().remove();
+	// console.log("User ref set to " + userRef);
+}
+
+function disconnectUser() {
+	if (isSignedIn == true) {
+		$.ajax({
+			url: config.databaseURL + "/testAjax/users/" + userKey.name + "/.json",
+			data: userKey,
+			type: "DELETE",
+			success: function() {
+				console.log(userName + " at key " + userKey + " erased from Firebase");
+			},
+			error: function(error) {
+				console.log("error: " +error);
+			}
+		});
+		return null;
+	}
+	else {
+		return null;
+	}
+}
+
+function clearRegisterForm() {
+	$("#userName").val("");
+	$("#userEmail").val("");
+	$("#userPassword").val("");
+}
+
+function clearLoginForm() {
+	$("#login-email").val("");
+	$("#login-password").val("");
+}
+
+function setUserName(email) {
+	var userNameUncased = email.substr(0, email.indexOf('@'));
+		userName = userNameUncased.toLowerCase();
+}
+
+// Section 3:
+// functions
+// =============================================================================
+
+// When the user clicks the registration button..
+// $("#register_Me").on("click", function(){
+// 	showRegistration();
+// 	hideLogin();
+// });
+
+// 
+// LOGIN USER
+// When the user clicks the login button in the login form..
+$("#user-Login").on("click", function(event){
+	// Prevent default browser form behavior
+	event.preventDefault();
+
+	var loginEmail = $("#loginEmail").val().trim();
+	var loginPassword = $("#loginPassword").val().trim();
+
+	if (loginEmail !== "" && loginPassword !== "") {
+
+		setUserName(loginEmail);
+
+		// pass user login info to firebase
+		firebase.auth().signInWithEmailAndPassword(loginEmail, loginPassword).catch(function(error) {
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  console.log(errorCode);
+		  console.log(errorMessage);
+		});
+
+		auth.onAuthStateChanged(function(user){
+		if (user) {
+			console.log("You are signed in");
+			console.log(user);
+			setUser();
+			// Get latest location and update firebase with user's lat and lng
+			updateLocation();
+			// Starts location update timer
+			setLocationTimer();
+			$("#loginModal").modal("hide");
+			checkUser();
+		} else {
+			console.log("Your are not signed in");
+			console.log("Please log in");
+		}
+	});
+
+	}
+	else {
+		console.log("Login form missing some information.");
+		$("#loginMessage").text("No fields can be empty!");
+	}
+	
+});
+
+// CREATE USER:
+// When the user clicks the submit button in the registration form..
+$("#user-SignUp").on("click", function(event){
+	// prevent default behavior
+	event.preventDefault();
+
+	// grab the users form information and save to variables
+	var name = $("#userName").val().trim();
+	var userEmail = $("#userEmail").val().trim();
+	var userPassword = $("#userPassword").val().trim();
+
+	if (name !== "" && userEmail !== "" && userPassword !== "") {
+
+		setUserName(userEmail);
+
+		// tests and debugging
+		console.log(userName);
+		console.log(name);
+		console.log(userEmail);
+		console.log(userPassword);
+
+		// Tempory JSON variable to hold the user information
+		var newUser = {
+			name: name,
+			email: userEmail,
+		};
+
+		userSlot = totalUsers + 1;
+
+		// send user login information to firebase
+		db.ref().child('users/' + userName + '/name').set(name);
+		db.ref().child('users/' + userName + '/email').set(userEmail);
+		
+		// Create user in firebase authentication
+		auth.createUserWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
+			var errorCode = error.code;
+			var errorMessage = error.message;
+		});
+		
+		// Use firebase authentication listner to show current logged in user
+		auth.onAuthStateChanged(function(user){
+			if (user) {
+				console.log("You are signed in");
+				console.log(user);
+				setUser();
+				// Get latest location and update firebase with user's lat and lng
+				updateLocation();
+				// Starts location update timer
+				setLocationTimer();
+				$("#loginModal").modal("hide");
+				checkUser();
+			} else {
+				console.log("Your are not signed in");
+				console.log("Please log in");
+			}
+		});
+
+		// alert user of signUp
+		console.log("Account successfully added!")
+
+		// clear the form
+		clearRegisterForm();
+
+		// Show logout button when user logs in
+		$("#user-Logout").show();
+
+		// Get latest location and update firebase with user's lat and lng
+		updateLocation();
+		// Starts location update timer
+		setLocationTimer();
+		$("#registerModal").modal("hide");
+	}
+	else {
+		console.log("Register form missing some information.");
+		$("#registerMessage").text("No fields can be empty!");
+	}
+});
+
+$("#cancel-register").on("click", function() {
+	clearRegisterForm();
+});
+
+$("#cancel-login").on("click", function() {
+	clearLoginForm();
+});
+
+// USER LOGOUT:
+// When the user clicks the logout button
+$("#user-Logout").on("click", function(){
+	auth.signOut().then(function() {
+		console.log("Sign-out successful.");
+		signOutUser();
+	}).catch(function(error){
+		console.log("An error has occured when logging out.");
+		console.log(error);
+	})
+});
+
+// Section 4:
+// Main process
+// ================================================================================
+
+// Hide It and Seeker screens and display Login screen on startup
+hideItScreen();
+hideSeekerScreen();
+hideLogoutButton();
+showLoginScreen();
+hideTagDiv();
+
+// Get the total number of users
+db.ref().on("value", function(snapshot) {
+	totalUsers = snapshot.child('users').numChildren();
+});
---
+++ assets/javascript/maps.js
@@ -0,0 +1,342 @@
+var map;
+var infoWindow;
+// Array to hold the list of breadcrumbs
+var crumbList = new Array();
+// Configurable value to control how many breadcrumbs are shown
+var crumbsToShow = 10;
+// Array to hold the breadcrumb markers
+var markersArray = [];
+// Global marker variable to help with click events
+var locationArray = [];
+// Boolean for tracking whether the user's location has been initially logged
+var locationLogged = new Boolean(false);
+// Object for handling the user's position
+var pos = {};
+// Object for handling It's position
+var posIt = {};
+// Location Update Timer
+var locationTimer;
+// Distance between the user and IT in miles
+var distanceToIt;
+// Distance between the user and IT in meters
+var distanceToItMeters
+// Target distance for being able to TAG IT
+var targetDistance = 0.001;
+
+// Function that initializes the map
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 17,
+		streetViewControl: false
+});
+
+infoWindow = new google.maps.InfoWindow;
+
+	// Get browser Geolocation
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition( function(position) {
+			// Set position to the browser's location
+			pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+
+			infoWindow.setPosition(pos);
+
+			map.setCenter(pos);
+		}, function() {
+			handleLocationError(true, infoWindow, map.getCenter());
+		});
+	} 
+	else {
+		// Browser doesn't support Geolocation
+		handleLocationError(false, infoWindow, map.getCenter());
+	}
+}
+
+// Centers the map
+function mapCenter() {
+	map.setCenter(pos);
+}
+
+// Updates variable pos with the user's current geolocation coordinates
+function getLocation() {
+	// Get browser Geolocation
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition( function(position) {
+			// Set position to the browser's location
+			pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+
+			locationLogged = true;
+			// return pos;
+		}, function() {
+			handleLocationError(true, infoWindow, map.getCenter());
+		});
+	}
+	else {
+		// Browser doesn't support Geolocation
+		handleLocationError(false, infoWindow, map.getCenter());
+	}
+}
+
+// Updates firebase with current user's pos location.
+function setLocation() {
+	if (isSignedIn == true && isIt == true) {
+		db.ref().child('connectedUsers/' + userName + '/pos/lat').set(pos.lat);
+		db.ref().child('connectedUsers/' + userName + '/pos/lng').set(pos.lng);
+		db.ref().child('itList/' + userName + '/pos/lat').set(pos.lat);
+		db.ref().child('itList/' + userName + '/pos/lng').set(pos.lng);
+		console.log("Updated user location to " + pos.lat + " / " + pos.lng);
+	}
+	else if (isSignedIn == true && isIt == false) {
+		db.ref().child('connectedUsers/' + userName + '/pos/lat').set(pos.lat);
+		db.ref().child('connectedUsers/' + userName + '/pos/lng').set(pos.lng);
+		console.log("Updated user location to " + pos.lat + " / " + pos.lng);
+	}
+	else {
+		console.log("ERROR: Tried to set geolocation while not signed in.");
+	}
+}
+
+// Sets the current time to a local variable
+function setConnectTime() {
+	var now = moment().format("YYYYMMDDHmmss");
+	if (isSignedIn == true && isIt == true) {
+		db.ref().child('connectedUsers/' + userName + '/lastConnected').set(now);
+		db.ref().child('itList/' + userName + '/lastConnected').set(now);
+	}
+	else if (isSignedIn == true && isIt == false) {
+		db.ref().child('connectedUsers/' + userName + '/lastConnected').set(now);
+	}
+	else {
+		console.log("ERROR: Tried to set connection time while not signed in.");
+	}
+}
+
+// Gets the user's location and updates it in firebase.
+function updateLocation() {
+	console.log("Updating Location");
+	getLocation();
+	setLocation();
+	getLocationText(pos.lat, pos.lng);
+	removeLocationMarker();
+	setConnectTime();
+	addMarker(pos.lat, pos.lng, 'userLocation');
+
+	if (isIt == false) {
+		getDistance();
+		updateItDistance();
+		checkTagDistance();
+	}
+}
+
+function checkTagDistance() {
+	if (distanceToItMeters < targetDistance && itOnlineStatus == true) {
+		console.log(userName + " is close enough to TAG IT!");
+		$('#tagMessage').text("You are CLOSE enough to it!");
+		showTagDiv();
+	}
+}
+
+// Updates the user's location on a set interval if signed in.
+function setLocationTimer() {
+	if (isSignedIn === true) {
+		console.log("Starting location update timer.");
+		locationTimer = setInterval(updateLocation, locationInterval);
+	}
+	else {
+		console.log("Not signed in. Location not registered");
+	}
+}
+
+// Removes the markers stored in markersArray from the map.
+function clearOverlays() {
+	for (i = 0; i < markersArray.length; i++) {
+		markersArray[i].setMap(null);
+	}
+	markersArray.length = 0;
+}	
+
+// Creates an error window if geolocation fails or the browser does not support geolocation
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(browserHasGeolocation ?
+		'Error: The Geolocation service failed.' :
+		'Error: Your browser doesn\'t support geolocation.');
+	infoWindow.open(map);
+}
+
+// Adds new breadcrumb to breadcrumbList object in Firebase
+function addCrumb(user, lat, lng, url) {
+	var breadcrumb = {
+		user: userName,
+		lat: lat,
+		lng: lng,
+		url: url
+	};
+
+	// If there are fewer index entries than the variable crumbsToShow, just add the new breadcrumb
+	if (!crumbList) {
+		crumbList = new Array(breadcrumb);
+	}
+	else if (crumbList.length < crumbsToShow) {
+		crumbList.push(breadcrumb); 
+	}
+	// If number of index entries is equal or greater than crumbsToShow, remove the first entry and then add the new breadcrumb
+	else {
+		crumbList.shift();
+		crumbList.push(breadcrumb);
+	}
+
+	db.ref('breadcrumbList').set(crumbList);
+}
+
+// Adds a new map marker to the map
+function addMarker(lat, lng, feature, image) {
+	// Sets marker latitude and longitude
+	var myLatLng = {lat: lat, lng: lng};
+
+	// Icons based on type for the map
+	var icons = {
+		breadcrumb: {
+			url: 'assets/images/bread-flat.png',
+			scaledSize: new google.maps.Size(30, 30),
+			origin: new google.maps.Point(0, 0),
+			anchor: new google.maps.Point(0, 15),
+			title: 'Breadcrumb',
+			zIndex: 0,
+		},
+		userLocation: {
+			url: 'assets/images/youarehere.png',
+			scaledSize: new google.maps.Size(30, 30),
+			origin: new google.maps.Point(0, 0),
+			anchor: new google.maps.Point(0, 15),
+			title: 'You are here',
+			zIndex: 1,
+		}
+	};
+
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		icon: icons[feature],
+		title: icons[feature].title,
+		image: image
+	});
+
+	if (feature == 'breadcrumb') {
+		markersArray.push(marker);
+	}
+	else {
+		locationArray.push(marker);
+	}
+}
+
+function removeLocationMarker() {
+	if (locationLogged == true) {
+		locationArray[0].setMap(null);
+		locationArray = [];
+	}
+}
+
+// Updates the Seeker screen to show distance to IT's location
+function updateItDistance() {
+	if (itOnlineStatus == true) {
+		$('#itDistance').text("You are " + distanceToIt + " miles from IT's current location.");
+		console.log("User is " + distanceToIt + " miles from IT's current location.");
+	}
+	else {
+		$('#itDistance').text("You are " + distanceToIt + " miles from IT's last known location.");
+		console.log("User is " + distanceToIt + " miles from IT's last known location.");
+	}
+}
+
+// Use the Haversine formula to detect distance in meters between two coordinates
+var rad = function(x) {
+	return x * Math.PI / 180;
+}
+
+var getDistance = function() {
+	var R = 6378137; // Earth's mean radius in meters
+	var dLat = rad(posIt.lat - pos.lat);
+	var dLng = rad(posIt.lng - pos.lng);
+	var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(rad(pos.lat)) * Math.cos(rad(posIt.lat)) * Math.sin(dLng / 2) * Math.sin(dLng / 2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	var d = R * c; // d = distance in meters
+	var m = d * 0.00062137; // Convert meters to miles
+	distanceToItMeters = d;
+	distanceToIt = m.toFixed(2);
+	updateItDistance();
+	return m.toFixed(2); // returns the distance in miles
+}
+
+function getLocationText(lat, lng) {
+	var pos = lat + ',' + lng;
+
+	$.ajax({
+		url: 'https://maps.googleapis.com/maps/api/geocode/json',
+		data: {
+			'latlng': pos,
+		},
+		dataType: 'json',
+		success: function(r) {
+			var text = r.results[0].formatted_address;
+			setLocationText(text);
+		},
+		error: function(e) {
+			console.log('ERROR: ', e);
+		}
+	})
+}
+
+function setLocationText(text) {
+	$('#locationText').text("Current Location: " + text);
+}
+
+$('.breadcrumbList').on("click", ".crumb", function() {
+	var lat = $(this).attr('data-lat');
+	var lng = $(this).attr('data-lng');
+	var latLng = new google.maps.LatLng(lat, lng);
+	map.setCenter(latLng);
+	console.log("Centering map to Lat: " + lat + " / Lng: " + lng);
+});
+
+$("#submit-crumb").on("click", function() {
+	$("#modalImage").empty();
+	$("#crumbMsg").empty();
+	$("#myModal").modal("hide");
+	imageReady = false;
+	getLocation();
+	addCrumb(userName, pos.lat, pos.lng, url);
+});
+
+$("#cancel-crumb").on("click", function() {
+	$("#modalImage").empty();
+	$("#crumbMsg").empty();
+});
+
+// At Launch or when breadcrumbList is updated, adds marker for each of last 10 breadcrumbs
+db.ref('breadcrumbList').on("value", function(snapshot) {
+	crumbList = snapshot.val();
+	clearOverlays();
+	
+	if (snapshot.exists()) {
+		console.log("There are " + crumbList.length + " total breadcrumbs.");
+		// Function that generates the crumb list on the map and Seeker page
+		generateCrumbs();
+	}
+});
+
+db.ref('itList').on("value", function(snapshot) {
+	if (snapshot.exists()) {
+		var itList = snapshot.val();
+		var itListKeys = Object.keys(itList);
+		var itName = itListKeys[0];
+		posIt = itList[itName].pos;
+		console.log("IT's location is Lat: " + posIt.lat + "/ Lng: " + posIt.lng);
+	}
+});
